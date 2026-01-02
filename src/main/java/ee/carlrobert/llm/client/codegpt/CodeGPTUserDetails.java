82cd254199e1e8ae6325dbd482a2699cd6691c43
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2026-01-02 15:12:25 +0200

    feat: add credit fields to CodeGPTUserDetails

--- src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTUserDetails.java
+++ src/main/java/ee/carlrobert/llm/client/codegpt/CodeGPTUserDetails.java
@@ -10,6 +10,8 @@ public class CodeGPTUserDetails {
 
   private final String fullName;
   private final PricingPlan pricingPlan;
+  private final Long creditsUsed;
+  private final Long creditsTotal;
   private final List<AvailableModel> availableModels;
   private final List<AvailableModel> toolWindowModels;
   private final String avatarBase64;
@@ -18,11 +20,15 @@ public class CodeGPTUserDetails {
   public CodeGPTUserDetails(
       @JsonProperty("fullName") String fullName,
       @JsonProperty("pricingPlan") PricingPlan pricingPlan,
+      @JsonProperty("creditsUsed") Long creditsUsed,
+      @JsonProperty("creditsTotal") Long creditsTotal,
       @JsonProperty("availableModels") List<AvailableModel> availableModels,
       @JsonProperty("toolWindowModels") List<AvailableModel> toolWindowModels,
       @JsonProperty("avatarBase64") String avatarBase64) {
     this.fullName = fullName;
     this.pricingPlan = pricingPlan;
+    this.creditsUsed = creditsUsed;
+    this.creditsTotal = creditsTotal;
     this.availableModels = availableModels;
     this.toolWindowModels = toolWindowModels;
     this.avatarBase64 = avatarBase64;
@@ -34,6 +40,14 @@ public class CodeGPTUserDetails {
 
   public PricingPlan getPricingPlan() {
     return pricingPlan;
+  }
+
+  public Long getCreditsUsed() {
+    return creditsUsed;
+  }
+
+  public Long getCreditsTotal() {
+    return creditsTotal;
   }
 
   public List<AvailableModel> getAvailableModels() {
